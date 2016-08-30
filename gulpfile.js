d43365c59e944d1c37f6521e4e8e34cea257d4dc
Thomas Hare <tommy@Tommys-MacBook-Pro.local>
2016-08-29 21:23:12 -0700

    Added custom settings, with ability to change grid display in the future

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ var rename = require('gulp-rename');
 gulp.task('default', ['slate-min', 'watch']);
 
 gulp.task('watch', function() {
-  return gulp.watch('./styl/**/*.styl', ['slate-min']);
+  return gulp.watch(['./styl/**/*.styl', './custom/**/*.styl'], ['slate-min']);
 });
 
 gulp.task('slate', function() {
