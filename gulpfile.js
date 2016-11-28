fd0dd7fffaa19d83f9936e09ae4212f30c28791a
Thomas Hare <tommyhares@gmail.com>
2016-11-27 18:01:02 -0700

    Added changes to produce external sourcemaps in css.

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ let cssnano = require('gulp-cssnano');
 let rename = require('gulp-rename');
 let sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('default', ['slate-min']);
+gulp.task('default', ['slate','slate-min']);
 
 gulp.task('watch', function () {
   return gulp.watch(['styl/**/*.styl', 'custom/**/*.styl'], ['slate']);
@@ -20,11 +20,11 @@ gulp.task('slate', function () {
       this.end();
     }))
     .pipe(autoprefixer())
-    .pipe(sourcemaps.write())
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./css'));
 });
 
-gulp.task('slate-min', ['slate'], function () {
+gulp.task('slate-min', function () {
   return gulp.src('./styl/slate.styl')
     .pipe(sourcemaps.init())
     .pipe(stylus({ 'include css': true }))
@@ -33,6 +33,6 @@ gulp.task('slate-min', ['slate'], function () {
     .pipe(rename({
       extname: '.min.css'
     }))
-    .pipe(sourcemaps.write())
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./css'));
 });
