6c3836a7a3eb79702d09f013db3eced10dd9909b
Louis Sandoval <louis@sandoval.com>
2016-11-27 17:59:40 -0700

    Run slate-min and slate in parallel when running default task

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ let cssnano = require('gulp-cssnano');
 let rename = require('gulp-rename');
 let sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('default', ['slate-min']);
+gulp.task('default', ['slate-min', 'slate']);
 
 gulp.task('watch', function () {
   return gulp.watch(['styl/**/*.styl', 'custom/**/*.styl'], ['slate']);
@@ -24,7 +24,7 @@ gulp.task('slate', function () {
     .pipe(gulp.dest('./css'));
 });
 
-gulp.task('slate-min', ['slate'], function () {
+gulp.task('slate-min', function () {
   return gulp.src('./styl/slate.styl')
     .pipe(sourcemaps.init())
     .pipe(stylus({ 'include css': true }))
