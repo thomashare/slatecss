4d9b8d94245397d88ce2c7ef4a1432eab3a2c677
Louis Sandoval <louis@sandoval.com>
2017-03-23 22:22:19 -0700

    Remove creation of extra threads
    - Transpiling is now fast enough

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,7 @@
 let gulp = require('gulp');
 let stylus = require('gulp-stylus');
 let postcss = require('gulp-postcss');
-let forp = require('forp');
-let forkme = require('forkme');
+let sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('default', ['slate-min', 'slate']);
 
@@ -12,59 +11,32 @@ gulp.task('watch', function () {
 	return gulp.watch(['styl/**/*.styl', 'custom/**/*.styl'], ['slate']);
 });
 
-gulp.task('slate', function (done) {
-	/**
-	 * opens in a new thread
-	 */
-	forp(forkme(function () {
-		/**
-		 * re-import deps in this context
-		 */
-		let gulp = require('gulp');
-		let stylus = require('gulp-stylus');
-		let postcss = require('gulp-postcss');
-		let sourcemaps = require('gulp-sourcemaps');
-
-		gulp.src('./styl/slate.styl')
-			.pipe(sourcemaps.init())
-			.pipe(stylus({'include css': true}).on('error', function () {
-				this.end();
-			}))
-			.pipe(postcss([require('autoprefixer')]))
-			.pipe(sourcemaps.write('./'))
-			.pipe(gulp.dest('./css'));
-	})).then(done);
-
+gulp.task('slate', function () {
+	return gulp.src('./styl/slate.styl')
+		.pipe(sourcemaps.init())
+		.pipe(stylus({'include css': true}).on('error', function () {
+			this.end();
+		}))
+		.pipe(postcss([require('autoprefixer')]))
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest('./css'));
 });
 
-gulp.task('slate-min', function (done) {
-	/**
-	 * opens in a new thread
-	 */
-	forp(forkme(function () {
-		/**
-		 * re-import deps in this context
-		 */
-		let gulp = require('gulp');
-		let stylus = require('gulp-stylus');
-		let postcss = require('gulp-postcss');
-		let rename = require('gulp-rename');
-		let sourcemaps = require('gulp-sourcemaps');
+gulp.task('slate-min', function () {
+	let rename = require('gulp-rename');
 
-		gulp.src('./styl/slate.styl')
-			.pipe(sourcemaps.init())
-			.pipe(stylus({'include css': true}))
-			.pipe(postcss([
-				require('autoprefixer'),
-				require('cssnano')
-			]))
-			.pipe(rename({
-				extname: '.min.css'
-			}))
-			.pipe(sourcemaps.write('./'))
-			.pipe(gulp.dest('./css'));
-	})).then(done);
-
+	return gulp.src('./styl/slate.styl')
+		.pipe(sourcemaps.init())
+		.pipe(stylus({'include css': true}))
+		.pipe(postcss([
+			require('autoprefixer'),
+			require('cssnano')
+		]))
+		.pipe(rename({
+			extname: '.min.css'
+		}))
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest('./css'));
 });
 
 gulp.task('margins', function () {
