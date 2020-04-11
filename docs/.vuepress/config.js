abadb5cf31c0f9418290cf27d1299578a8b5ac46
Tommy Hare <tommy@Tommys-MacBook-Pro.local>
2020-04-11 14:38:36 -0700

    Initial V4 push.

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  title: 'SLATE.CSS',
+  description: 'Slate is an unopinionated CSS framework for rapid web layout and design.',
+  themeConfig: {
+    logo: '/img/icon.png',
+    nav: [
+      { text: 'Getting Started', link: '/' },
+      { text: 'Github', link: 'https://github.com/thomashare/slatecss' },
+      { text: 'CDN', link: 'https://unpkg.com/slatecss@latest/slate.css' },
+    ],
+    sidebarDepth: 2,
+    sidebar: 'auto'
+  }
+}
